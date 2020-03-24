b8329b2c19537466cc43fd187c3da2175a2a4251
Rafael Alves <alvesrafael17@hotmail.com>
2020-03-24 16:09:15 -0300

    finalizando backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,7 +2,7 @@ const connection = require('../database/connection');
 const crypto = require('crypto');
 
 module.exports = {
-  async postOng(req,res){
+  async create(req,res){
     const { name, email, whatsapp, city, uf } = req.body;
     const id = crypto.randomBytes(4).toString('HEX');
   
@@ -17,7 +17,7 @@ module.exports = {
   
     return res.json({ id });
   },
-  async getOngs(req,res) {
+  async index(req,res) {
     const ongs = await connection('ongs').select('*');
 
     return res.json(ongs)
